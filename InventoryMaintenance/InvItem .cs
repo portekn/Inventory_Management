1d5cbcc7a311010673adb4839572d1eba7e8a52d
Kelli Porter <Portekn@mail.uc.edu>
2021-09-11 17:59:10 -0400

    Step 1-2: Added Psudo Code

--- InventoryMaintenance/InvItem .cs
+++ InventoryMaintenance/InvItem .cs
@@ -6,5 +6,18 @@ namespace InventoryMaintenance
 {
     public static class InvItem
     {
+        //Propertys
+        //ItemNo - Gets or sets an int that contains the item’s number.
+        //Description - Gets or sets a string that contains the item’s description.
+        //Price - Gets or sets a decimal that contains the item’s price.
+
+        //Methods
+        /*GetDisplayText() - Returns a string that contains the item’s number, 
+                             description, and price formatted like this:3245649    
+                             Agapanthus ($7.95). (The item number and description 
+                             are separated by four spaces.)*/
+        //Constructor
+        //() - Creates an InvItemobject with default values.
+        //(itemNo, description, price) - Creates an InvItemobject withthe specified values.
     }
 }
